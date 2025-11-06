43c55726f5b8dba8a09ccc31dfd35f6075a51621
Aryan25 <morearyan325@gmail.com>
2025-11-06 20:14:48 +0530

    esp 1

---
+++ App.js
@@ -0,0 +1,17 @@
+const parent = React.createElement(
+    "div",
+    {id: "parent"},
+    [React.createElement("div", {id: "child1"},
+        [React.createElement("h1", {}, "I am h1 tag inside child1 div"),
+        React.createElement("h2", {}, "I am h2 tag inside child1 div")]
+),
+    React.createElement("div", {id: "child2"},
+        [React.createElement("h1", {}, "I am h1 tag inside child2 div"),
+        React.createElement("h2", {}, "I am h2 tag inside child2 div")]
+    )]
+);
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
